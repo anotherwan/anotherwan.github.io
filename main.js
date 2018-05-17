d3867692ef5146897853bb9915ae885fc59a727c
swan <itsanotherwan@gmail.com>
2018-05-17 15:50:39 -0400

    Added ScrollMagic

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
-TweenMax.to(".s", 3, {
-    x:90,
-    y:220,
+TweenMax.to(".s", 5, {
+    x:5,
+    y:90,
     rotation:-90,
     delay:0.5,
-    ease:Elastic.easeOut
+    // scale:0.8,
+    ease:Power4.easeOut
+});
+
+TweenMax.to(".w", 6, {
+    x:140,
+    y:0,
+    scale:0.8,
+    ease:Power4.easeOut
+});
+
+TweenMax.to(".dot", 6, {
+    x:100,
+    y:0,
+    scale:0.8,
+    ease:Power4.easeOut
 });
